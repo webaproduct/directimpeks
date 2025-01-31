6e7ac38c2129fe749ee5d176cae5f477a8a99639
Oleg Usenko <oleg911usenko@gmail.com>
2025-01-31 18:51:25 +0200

    added empty module oleg_weba_directimpeks

---
+++ oleg_weba_directimpeks/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "oleg_weba_directimpeks",
+    "summary": """
+        """,
+    "author": "Oleg (Weba)",
+    "website": "https://weba.com.ua/",
+    "category": "Uncategorized",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        # "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
