13aa85e90c8c91ec636d0d20df6a3d70c6b32809
Oleg Usenko <oleg911usenko@gmail.com>
2025-05-14 13:47:06 +0300

    update oleg_weba_directimpeks

--- oleg_weba_directimpeks/__manifest__.py
+++ oleg_weba_directimpeks/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Oleg (Weba)",
     "website": "https://weba.com.ua/",
     "category": "Uncategorized",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "license": "LGPL-3",
     "depends": ["product"],
     "data": [
